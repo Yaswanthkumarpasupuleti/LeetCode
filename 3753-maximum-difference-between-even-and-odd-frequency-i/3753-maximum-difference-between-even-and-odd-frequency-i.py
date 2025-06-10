7d702b8ba4d829a06207110113067f95cc0b7734
Yaswanth Kumar Pasupuleti <yaswanthkumarpasupulati@gmail.com>
2025-06-10 09:37:35 +0530

    Time: 2 ms (69.59%), Space: 17.8 MB (47.42%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,23 +1,24 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        # freq  = {}
-        # for char in s:
-        #     if char in freq:
-        #         freq[char] += 1
-        #     else:
-        #         freq[char] = 1
-        # oddmax = evenmax = 0
-        # for val in freq.values():
-        #     if (val % 2 != 0) and (val > oddmax):
-        #         oddmax = val
-        #     if (val % 2 == 0) and (val > evenmax):
-        #         evenmax = val
-        # return oddmax - evenmax  
-        c = Counter(s)
-        print(c)
-        maxOdd = max(x for x in c.values() if x % 2 == 1)
-        print(maxOdd)
-        minEven = min(x for x in c.values() if x % 2 == 0)
-        print(minEven)
-        return maxOdd - minEven      
+        freq  = {}
+        for char in s:
+            if char in freq:
+                freq[char] += 1
+            else:
+                freq[char] = 1
+        oddmax  = 0
+        evenmin = (2**31)-1
+        for val in freq.values():
+            if (val % 2 != 0) and (val > oddmax):
+                oddmax = val
+            if (val % 2 == 0) and (val < evenmin):
+                evenmin = val
+        return oddmax - evenmin  
+        # c = Counter(s)
+        # print(c)
+        # maxOdd = max(x for x in c.values() if x % 2 == 1)
+        # print(maxOdd)
+        # minEven = min(x for x in c.values() if x % 2 == 0)
+        # print(minEven)
+        # return maxOdd - minEven      
         
