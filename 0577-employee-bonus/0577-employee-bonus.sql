63859420253c33a625478352e00769324231e188
Yaswanth Kumar Pasupuleti <yaswanthkumarpasupulati@gmail.com>
2025-08-08 09:46:10 +0530

    Time: 1022 ms (37.77%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT E.name,B.bonus FROM Employee E 
+LEFT JOIN Bonus B ON E.empID = B.empID
+WHERE bonus < 1000 OR Bonus IS NULL ;
