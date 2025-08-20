7f1aa3642634ad3022ff92774264ebc654876692
Yaswanth Kumar Pasupuleti <yaswanthkumarpasupulati@gmail.com>
2025-08-20 21:56:53 +0530

    Time: 3 ms (65.21%), Space: 19.8 MB (21.13%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor_operator = 0
-        for ele in nums:
-            xor_operator ^= ele
-        return xor_operator 
-        # freq = {}
+        # xor_operator = 0
         # for ele in nums:
-        #     if ele in freq:
-        #         freq[ele] += 1
-        #     else:
-        #         freq[ele] = 1
-        # for key,val in freq.items():
-        #     if val == 1:
-        #         return key
+        #     xor_operator ^= ele
+        # return xor_operator 
+        freq = {}
+        for ele in nums:
+            if ele in freq:
+                freq[ele] += 1
+            else:
+                freq[ele] = 1
+        for key,val in freq.items():
+            if val == 1:
+                return key
 
         
